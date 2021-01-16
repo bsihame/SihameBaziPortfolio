c275f33e6ba120b63d3f1e2449599e5c4ba5659f
bsihame <sihamebazi@pursuit.org>
2021-01-16 18:48:12 -0500

    delete images file from components

--- portfolio/src/components/HomePage.js
+++ portfolio/src/components/HomePage.js
@@ -3,13 +3,15 @@ import React, { Component } from 'react'
 export default class HomePage extends Component {
   render() {
     return (
-			<header id="main-header">
-				<div className="row no-gutters">
-					<div className=".col-lg-4 col-md-5"></div>
-					<img src="./images/SihameBazi.jpg" alt="profile picture" />
-				</div>
-				<div className=".col-lg-8 col-md-7"></div>
-			</header>
+			<>
+				<header id="main-header">
+					<div className="row no-gutters">
+						<div className=".col-lg-4 col-md-5"></div>
+						<img src="images/SihameBazi.jpg" alt="profile_picture" />
+					</div>
+					<div className=".col-lg-8 col-md-7"></div>
+				</header>
+			</>
 		);
   }
 }
